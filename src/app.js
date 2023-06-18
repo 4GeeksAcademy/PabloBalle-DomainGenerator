c4e71082b4d56d5445d23328154f805c82c6b3cb
Pablo Ballesteros Suarez <132361283+PabloBalle@users.noreply.github.com>
2023-06-18 15:20:42 +0000

    V1.0

--- src/app.js
+++ src/app.js
@@ -2,10 +2,66 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["joggeres", "racom"];
+let prefix = [".com", ".es", ".org", ".net"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function generateDomainNames(pronoun, adj, noun, prefix) {
+  let domainNames = [];
+
+  // Recorrer todas las listas de pronombres, adjetivos y sustantivos
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < prefix.length; l++) {
+          if (noun[k].endsWith("es") && prefix[l] === ".es") {
+            // Combinar los elementos de las listas y agregar el sufijo ".com"
+            let cleanNoun = noun[k].slice(0, -2);
+            // Combinar los elementos de las listas sin "com" y agregar el sufijo ".com"
+            let domainName = pronoun[i] + adj[j] + cleanNoun + prefix[l];
+            domainNames.push(domainName);
+          } else if (noun[k].endsWith("com") && prefix[l] === ".com") {
+            // Combinar los elementos de las listas y agregar el sufijo ".com"
+            let cleanNoun = noun[k].slice(0, -3);
+            // Combinar los elementos de las listas sin "com" y agregar el sufijo ".com"
+            let domainName = pronoun[i] + adj[j] + cleanNoun + prefix[l];
+            domainNames.push(domainName);
+          } else {
+            let domainName = pronoun[i] + adj[j] + noun[k] + prefix[l];
+            domainNames.push(domainName);
+          }
+        }
+      }
+    }
+  }
+  let randomDomain =
+    domainNames[Math.floor(Math.random(domainNames) * domainNames.length)];
+  return randomDomain;
+}
+
+console.log(generateDomainNames(pronoun, adj, noun, prefix));
+
+window.onload = () => {
+  let domainNameElement = document.getElementById("domainName");
+  let generateButton = document.getElementById("generateButton");
+
+  // Genera el nombre de dominio inicial y asígnalo al contenido del párrafo
+  domainNameElement.textContent = generateDomainNames(
+    pronoun,
+    adj,
+    noun,
+    prefix
+  );
+
+  // Agrega un listener de evento para el clic en el botón
+  generateButton.addEventListener("click", () => {
+    // Genera un nuevo nombre de dominio y asígnalo al contenido del párrafo
+    domainNameElement.textContent = generateDomainNames(
+      pronoun,
+      adj,
+      noun,
+      prefix
+    );
+  });
 };
